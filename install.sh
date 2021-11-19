285fc3ea6b70ae61ee891479f50d84676502cf64
Sparrow System <dangerdyke@protonmail.com>
2021-11-19 17:33:32 -0500

    removed npm shit that didn't work

--- install.sh
+++ install.sh
@@ -2,18 +2,13 @@
 # a script that will install foundryVTT locally and start it as a daemon,
 # assuming node.js and npm are installed.
 
-# dependencies
-echo "downloading and installing dependencies..."
-npm i pm2@latest -g
-echo "done"
-
 echo "downloading and installing foundryVTT..."
 mkdir foundryvtt
 mkdir foundrydata
 cd foundryvtt
-wget -0 foundryvtt.zip $1
+wget -O foundryvtt.zip $1
 unzip foundryvtt.zip
 echo "done"
 
-echo "install complete, starting foundryvtt as a daemon"
-pm2 start startup.sh
+echo "install complete, starting foundryvtt"
+node resources/app/main.js --dataPath=../foundrydata
